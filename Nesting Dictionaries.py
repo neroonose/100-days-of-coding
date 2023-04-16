6ab149a62b65fba28f2c1b34fbc2bf8afbe86e71
neroonose <neroonose@gmail.com>
2023-04-16 04:17:22 +0100

    Added to 100 days of code

---
+++ Nesting Dictionaries.py
@@ -0,0 +1,16 @@
+# Nesting Dictionary in a Dictionary
+travel_log = {
+  "France": {"cities_visited": ["Paris", "Lille", "Dijon"]},
+  "Germany": {"cities_visited": ["Berlin", "Hamburg", "Stuttgart"]}
+}
+#Nesting Dictionary in a List
+travel_log = [
+  {
+  "Country": "France", "cities_visited": ["Paris", "Lille", 
+  "Dijon"], "total_visits": 12
+  },
+  {
+    "Country": "Germany",
+    "cities_visited": ["Berlin", "Hamburg", "Stuttgart"], 
+    "total_visits": 5}
+]

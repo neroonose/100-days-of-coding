ea9fe9361c036d2a1c0669f4f9897b3c08d92695
neroonose <neroonose@gmail.com>
2023-05-08 07:56:01 +0100

    fixed bugs

--- guess_the_number.py
+++ guess_the_number.py
@@ -18,6 +18,7 @@ else:
 # Allow the player to submit a guess for a number between 1 and 100
 def game():
     the_guess = random.randint(1, 100)
+    print(f"computer guessed {the_guess}")
     end_of_game = False
 
     remaining_guesses = num_guesses
@@ -26,27 +27,26 @@ def game():
         print(f"You have {remaining_guesses} remaining.")
         user = int(input("pick a number between 1 and 100:"))
         if user > the_guess:
-            return "Too high"
+            remaining_guesses -= 1
+            #int(input("Guess Again!:"))
+            print( "Too high")
         elif user < the_guess:
-            return "Too low"
+            remaining_guesses -= 1
+            #int(input("Guess Again!:"))
+            print( "Too low")
         elif user == the_guess:
             print(f"You got it! the answer was {the_guess}")
+            end_of_game = True
         
-    remaining_guesses -= 1
-    if remaining_guesses == 0:
-        end_of_game = True
-        
-        
-print(game())
-
+        if remaining_guesses == 0:
+            end_of_game = True
+            print("You lost")
             
-    # If they got the answer correct, show the actual answer to the player.
-    # Track the number of turns remaining.
-    # If they run out of turns, provide feedback to the player. 
-    # Include two different difficulty levels (e.g., 10 guesses in easy mode, only 5 guesses in hard mode).
+        
+game()
 
-    #if mode == "hard":
-      #print("You have 5 attempts remaining to guess the number")
-    #elif mode == "easy":
-      #print("You have 10 attempts remaining to guess the number")
-      
+play_again = input("Do you want to play again? Type 'Y' or 'N':")            
+if play_again == "Y":
+    game()
+elif play_again == "N":
+    exit()

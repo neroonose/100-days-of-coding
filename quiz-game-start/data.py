2821e769f85740402a2173e67611ac242dd1c4c6
neroonose <neroonose@gmail.com>
2023-07-24 06:26:05 +0100

    formatted data file

--- quiz-game-start/data.py
+++ quiz-game-start/data.py
@@ -2,8 +2,35 @@ question_data = [
     {"category": "General Knowledge",
      "type": "boolean",
      "difficulty": "medium",
-     "question": ""Typewriter" is the longest word that can be typed using only the first row on a QWERTY keyboard.",
+     "question": "'Typewriter' is the longest word that can be typed using only the first row on a QWERTY keyboard.",
      "correct_answer": "True",
      "incorrect_answers": ["False"]},
-    {"category": "General Knowledge", "type": "boolean", "difficulty": "medium", "question": "The US emergency hotline is 911 because of the September 11th terrorist attacks.", "correct_answer": "False", "incorrect_answers": ["True"]}, {"category": "General Knowledge", "type": "boolean", "difficulty": "medium", "question": "The word "news" originates from the first letters of the 4 main directions on a compass (North, East, West, South).", "correct_answer": "False", "incorrect_answers": ["True"]}, {"category": "General Knowledge", "type": "boolean", "difficulty": "medium", "question": "The average woman is 5 inches \/ 13 centimeters shorter than the average man.", "correct_answer": "True", "incorrect_answers": ["False"]}, {"category": "General Knowledge", "type": "boolean", "difficulty": "medium", "question": "Albert Einstein had trouble with mathematics when he was in school.", "correct_answer": "False", "incorrect_answers": [
-        "True"]}, {"category": "General Knowledge", "type": "boolean", "difficulty": "medium", "question": ""Santa Claus" is a variety of melon.", "correct_answer": "True", "incorrect_answers": ["False"]}, {"category": "General Knowledge", "type": "boolean", "difficulty": "medium", "question": "The scientific name for the Southern Lights is Aurora Australis?", "correct_answer": "True", "incorrect_answers": ["False"]}, {"category": "General Knowledge", "type": "boolean", "difficulty": "medium", "question": "The British organisation CAMRA stands for The Campaign for Real Ale.", "correct_answer": "True", "incorrect_answers": ["False"]}, {"category": "General Knowledge", "type": "boolean", "difficulty": "medium", "question": "The commercial UK channel ITV stands for "International Television".", "correct_answer": "False", "incorrect_answers": ["True"]}, {"category": "General Knowledge", "type": "boolean", "difficulty": "medium", "question": ""Buffalo buffalo Buffalo buffalo buffalo buffalo Buffalo buffalo." is a grammatically correct sentence.", "correct_answer": "True", "incorrect_answers": ["False"]}]
+    {"category": "General Knowledge", "type": "boolean", "difficulty": "medium",
+     "question": "The US emergency hotline is 911 because of the September 11th terrorist attacks.",
+     "correct_answer": "False", "incorrect_answers": ["True"]},
+    {"category": "General Knowledge", "type": "boolean", "difficulty": "medium",
+     "question": "The word 'news' originates from the first letters of the 4 main directions on a compass (North, East, West, South).",
+     "correct_answer": "False", "incorrect_answers": ["True"]},
+    {"category": "General Knowledge", "type": "boolean", "difficulty": "medium",
+     "question": "The average woman is 5 inches \/ 13 centimeters shorter than the average man.",
+     "correct_answer": "True", "incorrect_answers": ["False"]},
+    {"category": "General Knowledge", "type": "boolean", "difficulty": "medium",
+     "question": "Albert Einstein had trouble with mathematics when he was in school.", "correct_answer": "False",
+     "incorrect_answers": [
+         "True"]},
+    {"category": "General Knowledge", "type": "boolean", "difficulty": "medium",
+     "question": "'Santa Claus' is a variety of melon.", "correct_answer": "True", "incorrect_answers": ['False']},
+    {"category": "General Knowledge", "type": "boolean", "difficulty": "medium",
+     "question": "The scientific name for the Southern Lights is Aurora Australis?", "correct_answer": "True",
+     "incorrect_answers": ["False"]},
+    {"category": "General Knowledge", "type": "boolean", "difficulty": "medium",
+     "question": "The British organisation CAMRA stands for The Campaign for Real Ale.",
+     "correct_answer": "True", "incorrect_answers": ["False"]}, {
+        "category": "General Knowledge", "type": "boolean", "difficulty": "medium",
+        "question": "The commercial UK channel ITV stands for 'International Television'.", "correct_answer": "False",
+        "incorrect_answers": ["True"]},
+    {"category": "General Knowledge", "type": "boolean",
+     "difficulty": "medium",
+     "question": "'Buffalo buffalo Buffalo buffalo buffalo buffalo Buffalo buffalo.' is a grammatically correct sentence.",
+     "correct_answer": "True", "incorrect_answers": [
+        "False"]}]

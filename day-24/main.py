25919ca429fcc9fec125d01dbbe203e8ee4d8571
neroonose <neroonose@gmail.com>
2023-08-16 16:23:47 +0100

    updated snake game

---
+++ day-24/main.py
@@ -0,0 +1,6 @@
+# with open("day-24\my_text.txt") as file: 
+#     contents = file.read()
+#     print(contents)
+
+with open("new_text.txt", mode="w") as file:
+    file.write("I love Dr Martins!")
--- snake-game/scoreboard.py
+++ snake-game/scoreboard.py
@@ -1,4 +1,5 @@
 from turtle import Turtle
+
 ALIGNMENT = "center"
 FONT = ("Courier", 20, "normal")
 
@@ -8,23 +9,26 @@ class ScoreBoard(Turtle):
     def __init__(self):
         super().__init__()
         self.score = 0
-        self.high_score = 0
+        with open("data.txt") as file:
+            self.high_score  = int(file.read())
         self.color("white")
         self.penup()
         self.goto(0, 270)
         self.hideturtle()
         self.update_scoreboard()
 
-    def update_scoreboard(self):        
+    def update_scoreboard(self):
         self.clear()
         self.write(f"Score: {self.score} High Score: {self.high_score}", align=ALIGNMENT, font=FONT)
 
     def reset(self):
         if self.score > self.high_score:
             self.high_score = self.score
+            with open("data.txt", mode="w") as file:
+                file.write(f"{self.high_score}")
         self.score = 0
         self.update_scoreboard()
-        
+
     def increase_score(self):
-        self.score +=1
+        self.score += 1
         self.update_scoreboard()

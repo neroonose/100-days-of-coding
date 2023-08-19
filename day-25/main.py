49dbdb20ac52a12d2794bee170b87958e066133d
neroonose <neroonose@gmail.com>
2023-08-19 22:09:29 +0100

    minor edit

--- day-25/main.py
+++ day-25/main.py
@@ -2,15 +2,61 @@
 #             data = data_file.readlines()
 #             print(data)
 
-import csv
+# import csv
 
-with open(f"day-25\weather_data.csv.csv") as data_file:
-    data = csv.reader(data_file)
-    temperatures = []
-    for row in data:
-        if row[1] != "temp":
-            temperatures.append(int(row[1]))
+# with open(f"day-25\weather_data.csv.csv") as data_file:
+#     data = csv.reader(data_file)
+#     temperatures = []
+#     for row in data:
+#         if row[1] != "temp":
+#             temperatures.append(int(row[1]))
 
-print(temperatures)
+# print(temperatures)
 
 import pandas
+
+# data = pandas.read_csv("day-25\weather_data.csv")
+# print(type(data))
+# # print(data["temp"])
+
+# data_dict = data.to_dict()
+# # print(data_dict)
+# temp_list = data["temp"].to_list()
+# print(temp_list)
+# print(data.temp.max())
+
+# print(data[data.temp == (data.temp.max())])
+
+# monday = data[data.day == "Monday"]
+# print(monday.condition)
+# monday_temp = int(monday.temp)
+# monday_temp_F = monday_temp * 9/5 +32
+# print(monday_temp_F)
+
+
+# create a dataframe from scratch
+
+# data_dict = {
+#     "students": ["Amy", "James", "Angela"],
+#     "score": [76, 56, 65]
+# }
+
+# data = pandas.DataFrame(data_dict)
+# # print(data)
+# data.to_csv("new_data.csv")
+
+data = pandas.read_csv("day-25\\2018_Central_Park_Squirrel_Census_-_Squirrel_Data.csv")
+gray_squirrels_count = len(data[data["Primary Fur Color"] == "Gray"])
+red_squirrels_count = len(data[data["Primary Fur Color"] == "Cinnamon"])
+black_squirrels_count = len(data[data["Primary Fur Color"] == "Black"])
+print(gray_squirrels_count)
+print(red_squirrels_count)
+print(black_squirrels_count)
+
+data_dict = {
+    "Fur Color": ["Gray", "Cinnamon", "Black"],
+    "Count": [gray_squirrels_count, red_squirrels_count, black_squirrels_count]
+}
+
+df = pandas.DataFrame(data_dict)
+df.to_csv("day-25\\squirrel_count_csv")
---
+++ us-states-game-start/main.py
@@ -0,0 +1,35 @@
+import turtle
+import pandas
+
+screen = turtle.Screen()
+screen.title("U.S. States Game")
+image = "us-states-game-start\\blank_states_img.gif"
+screen.addshape(image)
+turtle.shape(image)
+
+data = pandas.read_csv("us-states-game-start\\50_states.csv")
+states = data.state.to_list()
+score = 0
+correct_guesses = []
+states_to_learn = []
+while len(correct_guesses) < 50:
+    answer = screen.textinput(title=f"{score}/50 Guess the State", prompt="What's another state's name?")
+    answer_state = answer.title()
+    if answer_state == "Exit":
+        break
+    if answer_state in states:
+        t = turtle.Turtle()
+        t.hideturtle()
+        t.penup()
+        state_data = data[data.state == answer_state]
+        t.goto(int(state_data.x), int(state_data.y))
+        t.write(answer_state)
+        score += 1
+        correct_guesses.append(answer_state)
+
+for _ in states:
+    if _ not in correct_guesses:
+        states_to_learn.append(_)  
+
+df = pandas.DataFrame(states_to_learn)
+df.to_csv("us-states-game-start\\states_to_learn_csv")

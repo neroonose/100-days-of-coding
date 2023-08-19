1ebde790defebd80bfac51ad04d181e811336f63
neroonose <neroonose@gmail.com>
2023-08-19 22:07:53 +0100

    us-game-start

---
+++ day-25/main.py
@@ -0,0 +1,16 @@
+# with open(f"day-25\weather_data.csv.csv") as data_file:
+#             data = data_file.readlines()
+#             print(data)
+
+import csv
+
+with open(f"day-25\weather_data.csv.csv") as data_file:
+    data = csv.reader(data_file)
+    temperatures = []
+    for row in data:
+        if row[1] != "temp":
+            temperatures.append(int(row[1]))
+
+print(temperatures)
+
+import pandas

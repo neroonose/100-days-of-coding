9b339ab3275fee47328f531b0e8c93658fc14c6d
neroonose <neroonose@gmail.com>
2023-10-12 11:24:53 +0100

    Updated Password Manager code

--- password-manager/main.py
+++ password-manager/main.py
@@ -2,6 +2,7 @@ from tkinter import *
 from tkinter import messagebox
 from random import choice, randint, shuffle
 import pyperclip
+import json
 
 #----------- Password Generator-------- #
 
@@ -27,18 +28,52 @@ def save():
     pwrd = password_entry.get()
     site = website_entry.get()
     mail = email_entry.get()
-
-    
+    new_data = {
+        site: {
+            "email": mail,
+            "password": pwrd,
+        }
+    }
 
     if len(site) < 1 or len(pwrd) < 1:
         messagebox.showinfo(title="Error", message= "Hey!⚠️ Make sure you haven't left any fields empty!")
     else:
-        is_okay = messagebox.askokcancel(title=site, message=f"These are the details entered: \nEmail: {mail} "
-                           f"\nPassword: {pwrd} \nIs it okay to save?")
-        with open("password-manager\data.txt", mode="a") as file:
-            file.write(f"{site} | {mail} | {pwrd}\n")
+        # is_okay = messagebox.askokcancel(title=site, message=f"These are the details entered: \nEmail: {mail} "
+        #                    f"\nPassword: {pwrd} \nIs it okay to save?")
+        try:
+            with open("password-manager\data.json", mode="r") as data_file:
+                #Reading old data
+                data = json.load(data_file)
+         
+        except FileNotFoundError:
+            with open("password-manager\data.json", mode="w") as data_file:
+                #Saving updated data
+                json.dump(new_data, data_file, indent=4)
+
+        else:
+            #Updating old data with new data
+            data.update(new_data)
+
+            with open("password-manager\data.json", "w") as data_file:
+                #Saving updated data
+                json.dump(data, data_file, indent=4)
+        finally:
             password_entry.delete(0, END)
             website_entry.delete(0, END)
+
+def find_password():
+    site = website_entry.get()
+    try:
+        with open("password-manager\data.json", "r") as data_file:
+            data = json.load(data_file)
+            old_password= data[site]["password"]
+            email = data[site]["email"]
+    except FileNotFoundError:
+        messagebox.showinfo(title = "Error", message="No Data File Found") 
+    except KeyError: 
+        messagebox.showinfo(title = "Error", message="No details for the website exists.")
+    else:
+        messagebox.showinfo(title = site, message=f"Email: {email}\n Password: {old_password}") 
 
 window = Tk()
 window.title("Password Manager")
@@ -58,12 +93,12 @@ email.grid(column=0, row=2)
 password = Label(text="Password:", font=25)
 password.grid(column=0, row=3)
 
-website_entry = Entry(width=36)
-website_entry.grid(row =1, column=1, columnspan=2)
+website_entry = Entry(width=30)
+website_entry.grid(row =1, column=1)
 website_entry.focus()
 
-email_entry = Entry(width=36)
-email_entry.grid(row =2, column=1, columnspan=2)
+email_entry = Entry(width=35)
+email_entry.grid(row =2, column=1)
 email_entry.insert(0, "nero@gmail.com")
 
 password_entry = Entry(width=21)
@@ -74,6 +109,9 @@ add.grid(column=1, columnspan=2, row=4)
 
 generate_pswrd = Button(text="Generate Password", command=gen_paswrd)
 generate_pswrd.grid(column=2,row=3)
+
+search = Button(text="Search", width=15, command=find_password)
+search.grid(column=2, row=1)
 
 
 window.mainloop()
